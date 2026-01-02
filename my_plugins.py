a397af5df7ccfb1c4aca5acac4cc7eed4ba60ed3
1ureka <summerheadquarters001@gmail.com>
2026-01-02 23:05:47 +0800

    refactor: 重新組織專案結構，並新增 reloader.py

--- my_plugins.py
+++ my_plugins.py
@@ -1,37 +1,43 @@
 from PySide2 import QtGui, QtWidgets  # type: ignore
 import substance_painter as sp  # type: ignore
+import reloader
 
-import importlib
-import ui.radial_menu as radial_menu
-import logic.transform as transform
-import logic.randomize as randomize
-
-importlib.reload(radial_menu)
-importlib.reload(transform)
-importlib.reload(randomize)
-
-
-def on_menu_option_selected(option):
-    if option == "映射變換":
-        transform.main()
-    elif option == "隨機化種子":
-        randomize.main()
-
-
-parent = sp.ui.get_main_window()
-menu = radial_menu.RadialMenu(parent, ["映射變換", "隨機化種子"], on_menu_option_selected)
-
-open_menu_action = QtWidgets.QAction("開啟插件選單", parent)
-open_menu_action.setShortcut(QtGui.QKeySequence("Ctrl+Q"))
-open_menu_action.triggered.connect(lambda: menu.show_at_cursor())
-
+open_menu_action = None
+menu = None
 
 def start_plugin():
+    global open_menu_action, menu
+    reloader.perform_reload()
+
+    from ui.radial_menu import RadialMenu
+    from legacy import transform
+    import randomize
+
+    def on_menu_option_selected(option):
+        if option == "映射變換":
+            transform.main()
+        elif option == "隨機化種子":
+            randomize.main()
+
+
+    parent = sp.ui.get_main_window()
+    menu = RadialMenu(parent, ["映射變換", "隨機化種子"], on_menu_option_selected)
+
+    open_menu_action = QtWidgets.QAction("開啟插件選單", parent)
+    open_menu_action.setShortcut(QtGui.QKeySequence("Ctrl+Q"))
+    open_menu_action.triggered.connect(lambda: menu.show_at_cursor())
+
     sp.ui.add_action(sp.ui.ApplicationMenu.Edit, open_menu_action)
 
 
 def close_plugin():
     global open_menu_action, menu
-    sp.ui.delete_ui_element(open_menu_action)
-    menu.destroyed.connect(lambda: sp.logging.info("已正確釋放插件選單資源"))
-    menu.deleteLater()
+
+    if open_menu_action:
+        sp.ui.delete_ui_element(open_menu_action)
+        open_menu_action = None
+
+    if menu:
+        menu.destroyed.connect(lambda: sp.logging.info("已正確釋放插件選單資源"))
+        menu.deleteLater()
+        menu = None
---
+++ reloader.py
@@ -0,0 +1,25 @@
+import substance_painter as sp  # type: ignore
+import importlib
+import sys
+
+# 定義需要重載的模組順序（由底層到高層）
+MODULES_TO_RELOAD = [
+    "legacy.transform",
+    "transform.utils",
+    "transform.handle_fill",
+    "transform.handle_generator",
+    "transform",
+    "randomize",
+    "ui.texture_sets_select",
+    "ui.transform_result_dialog",
+    "ui.radial_menu",
+]
+
+def perform_reload():
+    for module_name in MODULES_TO_RELOAD:
+        if module_name in sys.modules:
+            sp.logging.info(f"Reloading module: {module_name}")
+            importlib.reload(sys.modules[module_name])
+        else:
+            # 如果模組還沒被載入過，就直接 import 它
+            importlib.import_module(module_name)
